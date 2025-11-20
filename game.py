4d87cae56744686a9cd5224557022fa37bcc231b
Metro <versions.acre455@passmail.com>
2025-11-20 17:15:04 -0600

    created starter files

---
+++ game.py
@@ -0,0 +1,12 @@
+import pygame
+
+pygame.init()
+
+pygame.display.set_mode()
+pygame.display.set_caption("caption")
+
+pygame.time.Clock()
+
+while True:
+    pygame.display.flip()
+    pygame.display.update()
